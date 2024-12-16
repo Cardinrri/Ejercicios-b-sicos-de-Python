8ed66e1d0ea4eacafd5712a7d0acdecb928c0640
Ricardo <ricardorobles.analytics@gmail.com>
2024-12-16 18:25:58 +0100

    Cambio de prueba para verificar Git y GitHub

--- Primer_ejer-Ciego2.py
+++ Primer_ejer-Ciego2.py
@@ -11,3 +11,5 @@ print(f"{Suma} es Mayor que 20 y menor que 40? {resultado1}")
 resultado2 = not (Resta == 50)
 print(f"El resultado de la resta, 100-45, es: {Resta}")
 print(f"¿{Resta} es diferente de 50? {resultado2}")
+
+# Este es un cambio de prueba para Git y GitHub
